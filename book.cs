7367bc2c5056103df6acf46c7704478c2063d8ac
Praisg <120684474+Praisg@users.noreply.github.com>
2023-05-14 15:24:21 +0200

    Add project files.

---
+++ book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class book //class called book
+    {
+        public string author;
+        public string title;
+        public int pages;
+
+        public book(string autho_r, string titl_e, int page_s) //constructor` a special method in a class
+        {
+            author = autho_r;
+            title = titl_e;
+            pages = page_s;
+
+        }
+    }
+   
+}
